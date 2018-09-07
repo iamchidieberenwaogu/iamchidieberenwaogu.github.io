5d3b7aeae463b59244b71ddd29c0173c2d7dfdbe
iamchidieberenwaogu <42776743+iamchidieberenwaogu@users.noreply.github.com>
2018-09-07 19:21:58 +0000

    last upload

--- main.js
+++ main.js
@@ -23,8 +23,7 @@ function result() {
 	var xItem = randomValueFromArray(insertX);
 	var yItem = randomValueFromArray(insertY);
 	var zItem = randomValueFromArray(insertZ);
-
-	newStory = newStory.replace(':insertx:', xItem);
+    
     newStory = newStory.replace(':insertx:', xItem);
     newStory = newStory.replace(':inserty:', yItem);
     newStory = newStory.replace(':insertz:', zItem);
